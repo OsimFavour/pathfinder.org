1871baefd9ab44a2c778d526352ef6012d8b3e8e
Osim Favour <famos204@gmail.com>
2023-03-29 21:51:38 +0100

    pathfinder.com

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from blog import app
 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0', port=5000, debug=True)
-    app.run(host='0.0.0.0', port=4000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
     # app.run(host="http://192.168.31.40:33455/", debug=True)
